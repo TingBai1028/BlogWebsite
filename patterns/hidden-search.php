1fd60a8e02d04aa5a74e24a628c6d30a1c9fea14
Yuting <z5535043@ad.unsw.edu.au>
2025-01-15 17:44:52 +1100

    update template

--- patterns/hidden-search.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Title: Search
- * Slug: pulitzer/hidden-search
- * Inserter: no
- */
-?>
-<!-- wp:search {"label":"<?php echo esc_attr_x( 'Search', 'search form label', 'pulitzer' ); ?>","showLabel":false,"placeholder":"<?php echo esc_attr_x( 'Search for...', 'search form placeholder', 'pulitzer' ); ?>","buttonText":"<?php echo esc_attr_x( 'Search', 'search button text', 'pulitzer' ); ?>","buttonPosition":"button-inside","buttonUseIcon":true} /-->
